fe0a37b5e0c44bf83bb2d50cbfe7c49d698aa23c
Johannes <jojo@lepoard.liba>
2013-01-14 18:29:37 +0100

    [routegraph] Correctly position ViewPoint labels

--- viewpoint.cpp
+++ viewpoint.cpp
@@ -9,21 +9,21 @@
 ViewPoint::ViewPoint(QGraphicsItem *parent, const qreal phi, const QString name)
     : QGraphicsItem(parent)
 {
-    // Add M_PI to the angle to convert to Qt’s coordinate system
-    // Angle 0 is pointing downward
-    this->m_angle = fmod(phi + M_PI, 2 * M_PI);
+    // Convert the angle from Zusi's coordinate system (0° = pointing up, positive ccw)
+    // to Qt's coordinate system (0° = pointing right, negative ccw)
+    this->m_angle = fmod(3 * M_PI / 2 - phi, 2 * M_PI);
 
     this->m_label = new Label(name, this);
     m_label->setPen(QPen(Qt::blue));
 
-    if (m_angle - M_PI < 0.0001 || m_angle <= M_PI / 2) {
-        m_label->setAlignment(Qt::AlignLeft | Qt::AlignBottom);
-    } else if (m_angle <= M_PI) {
-        m_label->setAlignment(Qt::AlignLeft | Qt::AlignTop);
-    } else if (m_angle <= 3 * M_PI / 2) {
-        m_label->setAlignment(Qt::AlignRight | Qt::AlignTop);
-    } else {
+    if (m_angle < M_PI / 2) {
         m_label->setAlignment(Qt::AlignRight | Qt::AlignBottom);
+    } else if (m_angle <= M_PI) {
+        m_label->setAlignment(Qt::AlignLeft | Qt::AlignBottom);
+    } else if (m_angle <= 3 * M_PI / 2) {
+        m_label->setAlignment(Qt::AlignLeft | Qt::AlignTop);
+    } else {
+        m_label->setAlignment(Qt::AlignRight | Qt::AlignTop);
     }
 }
 
@@ -38,8 +38,8 @@ void ViewPoint::paint(QPainter *painter, const QStyleOptionGraphicsItem *option,
     Q_UNUSED(widget);
 
     painter->setPen(Qt::blue);
-    painter->drawLine(0, 0, sin(m_angle + 0.5) * VIEWPOINTSIZE, cos(m_angle + 0.5) * VIEWPOINTSIZE);
-    painter->drawLine(0, 0, sin(m_angle - 0.5) * VIEWPOINTSIZE, cos(m_angle - 0.5) * VIEWPOINTSIZE);
+    painter->drawLine(0, 0, cos(m_angle + 0.5) * VIEWPOINTSIZE, sin(m_angle + 0.5) * VIEWPOINTSIZE);
+    painter->drawLine(0, 0, cos(m_angle - 0.5) * VIEWPOINTSIZE, sin(m_angle - 0.5) * VIEWPOINTSIZE);
 }
 
 QRectF ViewPoint::boundingRect() const
