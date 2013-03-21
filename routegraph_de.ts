8d5297b2aa698b01eaeb2682907af35333fe78e9
Johannes <jojo@leopard.liba>
2013-03-21 01:04:34 +0100

    Update German translation

--- routegraph_de.ts
+++ routegraph_de.ts
@@ -10,7 +10,7 @@
     </message>
     <message>
         <location filename="mainwindow.ui" line="44"/>
-        <location filename="mainwindow.cpp" line="406"/>
+        <location filename="mainwindow.cpp" line="435"/>
         <source>(Simulation time)</source>
         <translation>(Simulationszeit)</translation>
     </message>
@@ -25,7 +25,7 @@
         <translation>&amp;Ansicht</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="90"/>
+        <location filename="mainwindow.ui" line="94"/>
         <source>&amp;Trains</source>
         <translation>&amp;Züge</translation>
     </message>
@@ -34,227 +34,257 @@
         <translation type="obsolete">Ö&amp;ffnen</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="105"/>
+        <location filename="mainwindow.ui" line="109"/>
         <source>&amp;Open route</source>
         <translation>Strecke ö&amp;ffnen</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="108"/>
+        <location filename="mainwindow.ui" line="112"/>
         <source>Ctrl+O</source>
         <translation>Ctrl+O</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="116"/>
+        <location filename="mainwindow.ui" line="120"/>
         <source>&amp;Text scaling</source>
         <translation>&amp;Text skaliert mit</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="130"/>
+        <location filename="mainwindow.ui" line="134"/>
         <source>&amp;Signal names</source>
         <translation>&amp;Signalnamen</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="133"/>
+        <location filename="mainwindow.ui" line="137"/>
         <source>S</source>
         <translation>S</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="144"/>
+        <location filename="mainwindow.ui" line="148"/>
         <source>&amp;View point names</source>
         <translation>Namen der streckenseitigen &amp;Beobachtungspunkte</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="147"/>
+        <location filename="mainwindow.ui" line="151"/>
         <source>V</source>
         <translation>B</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="158"/>
+        <location filename="mainwindow.ui" line="162"/>
         <source>St&amp;arting point names</source>
         <translation>Namen der &amp;Aufgleispunkte</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="161"/>
+        <location filename="mainwindow.ui" line="165"/>
         <source>T</source>
         <translation>A</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="172"/>
+        <location filename="mainwindow.ui" line="176"/>
         <source>A&amp;nti-aliasing</source>
         <translation>&amp;Kantenglättung</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="177"/>
+        <location filename="mainwindow.ui" line="181"/>
         <source>Zoom &amp;in</source>
         <translation>&amp;Vergrößern</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="180"/>
+        <location filename="mainwindow.ui" line="184"/>
         <source>+</source>
         <translation>+</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="185"/>
+        <location filename="mainwindow.ui" line="189"/>
         <source>Zoom &amp;out</source>
         <translation>Ver&amp;kleinern</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="188"/>
+        <location filename="mainwindow.ui" line="192"/>
         <source>-</source>
         <translation>-</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="193"/>
+        <location filename="mainwindow.ui" line="197"/>
         <source>&amp;Fit to window</source>
         <translation>In Fenster &amp;einpassen</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="196"/>
+        <location filename="mainwindow.ui" line="200"/>
         <source>1</source>
         <translation>1</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="204"/>
+        <location filename="mainwindow.ui" line="208"/>
         <source>&amp;Reconnect to Zusi</source>
         <translation>Mit Zusi neu &amp;verbinden</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="215"/>
+        <location filename="mainwindow.ui" line="219"/>
         <source>&amp;Show trains</source>
         <translation>&amp;Züge anzeigen</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="218"/>
+        <location filename="mainwindow.ui" line="222"/>
         <source>Z</source>
         <translation>Z</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="229"/>
+        <location filename="mainwindow.ui" line="233"/>
         <source>Show train &amp;names</source>
         <translation>Zug&amp;nummern anzeigen</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="240"/>
+        <location filename="mainwindow.ui" line="244"/>
         <source>Show &amp;current speed</source>
         <translation>Aktuelle &amp;Geschwindigkeit anzeigen</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="248"/>
+        <location filename="mainwindow.ui" line="252"/>
         <source>Show &amp;target signal</source>
         <translation>Ziel&amp;signal anzeigen</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="259"/>
+        <location filename="mainwindow.ui" line="263"/>
         <source>Stati&amp;on names</source>
         <translation>&amp;Haltepunkte</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="262"/>
+        <location filename="mainwindow.ui" line="266"/>
         <source>H</source>
         <translation>H</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="270"/>
+        <location filename="mainwindow.ui" line="274"/>
         <source>Open &amp;capture</source>
         <translation>&amp;Aufzeichnung öffnen</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="273"/>
+        <location filename="mainwindow.ui" line="277"/>
         <source>Ctrl+Shift+O</source>
         <translation>Ctrl+Shift+O</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="281"/>
+        <location filename="mainwindow.ui" line="285"/>
         <source>&amp;Save capture</source>
         <translation>Aufzeichnung &amp;speichern</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="284"/>
+        <location filename="mainwindow.ui" line="288"/>
         <source>Ctrl+S</source>
         <translation>Ctrl+S</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="92"/>
+        <location filename="mainwindow.ui" line="299"/>
+        <source>&amp;Unreachable track elements</source>
+        <translation>&amp;Unerreichbare Streckenelemente</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="302"/>
+        <source>U</source>
+        <translation>U</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="310"/>
+        <source>Manual &amp;registers</source>
+        <translation>Manuelle &amp;Registerpunkte</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="313"/>
+        <source>R</source>
+        <translation>R</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="321"/>
+        <source>A&amp;utomatic registers</source>
+        <translation>A&amp;utomatische Registerpunkte</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="324"/>
+        <source>Shift+R</source>
+        <translation>Shift+R</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="102"/>
         <source>Open route</source>
         <oldsource>Open file</oldsource>
         <translation>Streckendatei öffnen</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="92"/>
+        <location filename="mainwindow.cpp" line="102"/>
         <source>Route files (*.str *.STR)</source>
         <translation>Streckendateien (*.str *.STR)</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="122"/>
-        <location filename="mainwindow.cpp" line="164"/>
-        <location filename="mainwindow.cpp" line="173"/>
-        <location filename="mainwindow.cpp" line="183"/>
-        <location filename="mainwindow.cpp" line="291"/>
-        <location filename="mainwindow.cpp" line="420"/>
-        <location filename="mainwindow.cpp" line="539"/>
-        <location filename="mainwindow.cpp" line="542"/>
-        <location filename="mainwindow.cpp" line="564"/>
-        <location filename="mainwindow.cpp" line="587"/>
-        <location filename="mainwindow.cpp" line="611"/>
+        <location filename="mainwindow.cpp" line="132"/>
+        <location filename="mainwindow.cpp" line="188"/>
+        <location filename="mainwindow.cpp" line="197"/>
+        <location filename="mainwindow.cpp" line="206"/>
+        <location filename="mainwindow.cpp" line="340"/>
+        <location filename="mainwindow.cpp" line="449"/>
+        <location filename="mainwindow.cpp" line="568"/>
+        <location filename="mainwindow.cpp" line="571"/>
+        <location filename="mainwindow.cpp" line="593"/>
+        <location filename="mainwindow.cpp" line="616"/>
+        <location filename="mainwindow.cpp" line="640"/>
         <source>Error</source>
         <translation>Fehler</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="148"/>
+        <location filename="mainwindow.cpp" line="172"/>
         <source>Open capture file</source>
         <translation>Aufzeichnungsdatei öffnen</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="148"/>
-        <location filename="mainwindow.cpp" line="200"/>
+        <location filename="mainwindow.cpp" line="172"/>
+        <location filename="mainwindow.cpp" line="228"/>
         <source>Capture files (*.zusicapture *.ZUSICAPTURE)</source>
         <translation>Aufzeichnungsdateien (*.zusicapture *:ZUSICAPTURE)</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="164"/>
+        <location filename="mainwindow.cpp" line="188"/>
         <source>Wrong file format</source>
         <translation>Falsches Dateiformat</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="173"/>
+        <location filename="mainwindow.cpp" line="197"/>
         <source>Unsupported capture format version: %1</source>
         <translation>Nicht unterstützte Version des Aufzeichnungsformates: %1</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="200"/>
+        <location filename="mainwindow.cpp" line="228"/>
         <source>Save capture file</source>
         <translation>Aufzeichnungsdatei speichern</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="291"/>
-        <location filename="mainwindow.cpp" line="420"/>
+        <location filename="mainwindow.cpp" line="340"/>
+        <location filename="mainwindow.cpp" line="449"/>
         <source>Error while executing the following action: &quot;%1&quot;
 Message: &quot;%2&quot;</source>
         <translation>Fehler beim Ausführen der folgenden Aktion: „%1“
 Fehlermeldung: „%2“</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="539"/>
+        <location filename="mainwindow.cpp" line="568"/>
         <source>Error retrieving Zusi window handle: %1</source>
         <translation>Fehler beim Ermitteln des Fenster-Handles von Zusi: %1</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="542"/>
+        <location filename="mainwindow.cpp" line="571"/>
         <source>Could not find Zusi main window.</source>
         <translation>Zusi-Hauptfenster nicht gefunden.</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="564"/>
+        <location filename="mainwindow.cpp" line="593"/>
         <source>Could not bring Zusi window to foreground.</source>
         <translation>Kann das Zusi-Hauptfenster nicht in den Vordergrund bringen.</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="587"/>
+        <location filename="mainwindow.cpp" line="616"/>
         <source>Could not set focus to Zusi window.</source>
         <translation>Kann den Eingabefokus nicht auf das Zusi-Fenster setzen.</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="611"/>
+        <location filename="mainwindow.cpp" line="640"/>
         <source>Could not find Zusi train selection window.</source>
         <translation>Zusi-Zugauswahlfenster nicht gefunden.</translation>
     </message>
@@ -262,29 +292,72 @@ Fehlermeldung: „%2“</translation>
 <context>
     <name>QObject</name>
     <message>
-        <location filename="model/route.cpp" line="145"/>
+        <location filename="model/route.cpp" line="196"/>
+        <source>Unsupported file format</source>
+        <translation>Nicht unterstütztes Dateiformat</translation>
+    </message>
+    <message>
+        <location filename="model/route.cpp" line="198"/>
         <source>Unsupported file format version: %1</source>
         <translation>Nicht unterstütztes Dateiformat: Version %1</translation>
     </message>
     <message>
-        <location filename="graphicsitems/trainitem.cpp" line="156"/>
+        <location filename="graphicsitems/trainitem.cpp" line="157"/>
         <source>v = %1 kph</source>
         <translation>v = %1 km/h</translation>
     </message>
     <message>
-        <location filename="graphicsitems/trainitem.cpp" line="167"/>
+        <location filename="graphicsitems/trainitem.cpp" line="168"/>
         <source>-&gt; %1, track %2</source>
         <translation>→ %1, Gleis %2</translation>
     </message>
     <message>
-        <location filename="trainmanager.cpp" line="571"/>
+        <location filename="trainmanager.cpp" line="144"/>
         <source>.ls file of capture (%1) does not match .ls file of currently loaded route (%2)</source>
         <translation>.ls-Datei der Aufzeichnung (%1) stimmt nicht mit .ls-Datei der momentan geladenen Strecke (%2) überein </translation>
     </message>
     <message>
-        <location filename="trainmanager.cpp" line="577"/>
+        <location filename="trainmanager.cpp" line="150"/>
         <source>Track element count of capture (%1) does not match track element count of currently loaded route (%2)</source>
         <translation>Anzahl der Streckenelemente in der Aufzeichnungsdatei (%1) stimmt nicht mit der Anzahl der Streckenelemente der momentan geladenen Strecke (%2) überein</translation>
+    </message>
+    <message>
+        <location filename="trainmanager.cpp" line="425"/>
+        <source>Read Fahrstrasse for train %1 %2</source>
+        <translation>Lesen der Fahrstraße für Zug %1 %2</translation>
+    </message>
+    <message>
+        <location filename="trainmanager.cpp" line="426"/>
+        <source>Fahrstrasse end element number is 0</source>
+        <translation>Nummer des Fahrstraßen-Endelements ist 0</translation>
+    </message>
+    <message>
+        <location filename="graphicsitems/registermarker.cpp" line="6"/>
+        <source>Register %1</source>
+        <translation>Register %1</translation>
+    </message>
+</context>
+<context>
+    <name>SaveCaptureDialog</name>
+    <message>
+        <location filename="savecapturedialog.ui" line="17"/>
+        <source>Save capture</source>
+        <translation>Aufzeichnung speichern</translation>
+    </message>
+    <message>
+        <location filename="savecapturedialog.ui" line="25"/>
+        <source>Choose part of capture to save</source>
+        <translation>Zu speichernder Ausschnitt der Aufzeichnung</translation>
+    </message>
+    <message>
+        <location filename="savecapturedialog.ui" line="34"/>
+        <source>(Start time)</source>
+        <translation>(Startzeit)</translation>
+    </message>
+    <message>
+        <location filename="savecapturedialog.ui" line="48"/>
+        <source>(End time)</source>
+        <translation>(Endzeit)</translation>
     </message>
 </context>
 </TS>
